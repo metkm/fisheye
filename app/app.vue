6af799c2676538a1990f31beffd15bf9b4daf1fc
metkm <metinkorkmaz417@gmail.com>
2024-08-13 13:33:05 +0300

    center images vertically

--- app/app.vue
+++ app/app.vue
@@ -1,34 +1,33 @@
 <script setup lang="ts">
+import { AxesHelper } from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { IMAGE_HEIGHT, IMAGE_WIDTH, MAX_PER_LINE } from "./constants";
 import { createImageMesh, images } from "./urls";
 import { init } from "./init";
-import { IMAGE_HEIGHT, IMAGE_WIDTH, MAX_PER_LINE } from "./constants";
-import { AxesHelper } from "three";
 
 const { composer, camera, renderer, scene } = init();
 
 Promise.all(images.map((image) => createImageMesh(image))).then((cubes) => {
-  let offsetInitial = -((MAX_PER_LINE / 2) * IMAGE_WIDTH) + (IMAGE_WIDTH / 2)
+  const offsetInitial = -((MAX_PER_LINE / 2) * IMAGE_WIDTH) + (IMAGE_WIDTH / 2)
+  const lineCount = Math.ceil(images.length / MAX_PER_LINE);
 
   let offsetx = offsetInitial;
-  let offsety = -(Math.floor(images.length / MAX_PER_LINE) * IMAGE_HEIGHT) + (IMAGE_HEIGHT / 2);
-
-  console.log(offsety)
+  let offsety = (lineCount * IMAGE_HEIGHT / 2) - IMAGE_HEIGHT / 2;
 
   for (let index = 0; index < cubes.length; index++) {
     const cube = cubes[index];
     if (!cube) continue;
-
-    if (index % MAX_PER_LINE === 0) {
-      offsety += IMAGE_HEIGHT;
-      offsetx = offsetInitial;
-    }
 
     cube.position.x = offsetx;
     cube.position.y = offsety;
     
     scene.add(cube);
     offsetx += IMAGE_WIDTH;
+
+    if (index > 0 && (index + 1) % MAX_PER_LINE === 0) {
+      offsety -= IMAGE_HEIGHT;
+      offsetx = offsetInitial;
+    }
   }
 });
 
